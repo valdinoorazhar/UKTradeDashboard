2469d3a337fbbdcd4ef2cd826ff1edc53c24b567
valdinoorazhar <valdinoorazhar@gmail.com>
2026-07-13 06:14:36 +0700

    add trade import data ingestion

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
 import ingest_csv
-from datetime import datetime
 
-# Import dataset CSV
+# Declare file path
+file_path = '../data'
+
+# Ingest Export dataset CSV
 export_url = "https://www.ons.gov.uk/file?uri=/economy/nationalaccounts/balanceofpayments/datasets/uktradecountrybycommodityexports/current/countrybycommodityexports.xlsx"
-ingest_csv.download_csv(export_url, "trade_export.csv", "../data")
+export_file_name = 'trade_export.csv'
+ingest_csv.download_csv(export_url, export_file_name, file_path)
+
+# Ingest Import dataset CSV
+import_url = "https://www.ons.gov.uk/file?uri=/economy/nationalaccounts/balanceofpayments/datasets/uktradecountrybycommodityimports/current/countrybycommodityimports.xlsx"
+import_file_name = 'trade_import.csv'
+ingest_csv.download_csv(import_url, import_file_name, file_path)
